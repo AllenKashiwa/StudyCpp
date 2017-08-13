b25e35e3e96a64d6ae0966b7bfb0285e906effde
Allen Kashiwa <358804906@qq.com>
2017-08-13 16:05:36 +0800

    update 3.5

---
+++ C++ Primer 5th Edition/Chapter03/Exercise3.5/Exercise3.5.cpp
@@ -0,0 +1,62 @@
+﻿// 连接多个字符串
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    char cont;
+//    string s, result;
+//    cout << "请输入第一个字符串: " << endl;
+//    while (cin >> s)
+//    {
+//        result += s;
+//        cout << "是否继续(y or no)?" << endl;
+//        cin >> cont;
+//        if (cont == 'Y' || cont == 'y')
+//        {
+//            cout << "请输入下一个字符:" << endl;
+//        }
+//        else
+//        {
+//            break;
+//        }
+//    }
+//    cout << "结果字符串为:" << result << endl;
+//    return 0;
+//}
+
+// 连接字符串并以空格符分隔
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char cont;
+    string s, result;
+    cout << "请输入第一个字符串：" << endl;
+    while (cin >> s)
+    {
+        if (result.size())
+        {
+            s = " " + s;
+        }
+        result += s;
+        cout << "是否继续(y or n)?" << endl;
+        cin >> cont;
+        if (cont == 'Y' || cont == 'y')
+        {
+            cout << "请输入下一个字符串：" << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << "拼接后的字符串是：" << result << endl;
+    return 0;
+
+}
